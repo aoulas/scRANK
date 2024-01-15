fd02d338d66c302e42cf0513d2fdcdae404ce867
aoulas <anastasioso@cing.ac.cy>
2024-01-15 11:02:00 +0200

    Update

--- R/seuratBasic.R
+++ R/seuratBasic.R
@@ -1,11 +1,11 @@
-runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#scenario="Malacards",checkdrug=TRUE,keywordsWikiUser,keywordsKEGGUser,keywordsGOUser,keywordsMSIGUser,keywordsReactUser,keywordsMOAUser
+runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){
   library(enrichR)
-
+  
   if (missing(disease)) cat("Argument disease is missing") else cat(paste("Argument disease =", disease));
   cat ("\n");
   if (missing(path)) cat("Argument path is missing") else cat(paste("Argument path =", path));
   cat("\n\n");
-
+  
   if(annotate==TRUE){
     userlabel<-"label"
     usercelltype<-"celltype"
@@ -38,18 +38,16 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
   
   print(paste("Starting Analysis for",disease,sep=" "))
   
-
+  
   ##########################################Run analysis#############################################################################
   dirs <- list.dirs()
-
+  
   loaded.dataSO.list<-c()
-
+  
   plot.list<-vector("list", length(dirs)-1)
   index<-1
-  # if(disease=="MM"){
-  #   dirs<-dirs[c(1,2,13,20,27,28,29)] #changed this for mm
-  # }
-
+  
+  
   #remove directory with html files
   indexhtmlfiles<-grep("*_files",dirs)
   if(length(indexhtmlfiles) !=0){
@@ -59,11 +57,11 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
   #options(Seurat.object.assay.version = "v3")
   for(i in 2:length(dirs)){
     data_dir<-dirs[i]
-
+    
     filesindir<-list.files(data_dir)
     #check if dir contains seurat raw data using different formats (rds, H5, ect)
     foundfalse<-which(c("barcodes.tsv","genes.tsv","matrix.mtx") %in% filesindir==FALSE)
-
+    
     foundH5<-grep("\\.h5",filesindir)
     foundrds<-grep("\\.rds",filesindir)
     foundmeta<-"meta.txt"%in% filesindir
@@ -86,21 +84,21 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
       # VlnPlot(loaded.dataSO, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
       # FeatureScatter is typically used to visualize feature-feature relationships, but can be used
       # for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
-
+      
       #plot1 <- FeatureScatter(loaded.dataSO, feature1 = "nCount_RNA", feature2 = "percent.mt")
       plot2 <- FeatureScatter(loaded.dataSO, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-
+      
       plot.list[[index]]<-plot2
       index<-index+1
-
+      
       #Filtering data
       #loaded.dataSO <- subset(loaded.dataSO, subset = nFeature_RNA > 100 & nFeature_RNA < 6500 & percent.mt < 75)
-
+      
       #Normalizing the data. Normalized values are stored in UnWounded1[["RNA"]]@data. *
       loaded.dataSO <- NormalizeData(loaded.dataSO)#, normalization.method = "LogNormalize", scale.factor = 10000)
       #Identification of highly variable features (feature selection) *
       loaded.dataSO <- FindVariableFeatures(loaded.dataSO, selection.method = "vst", nfeatures = 2000) 
-
+      
       loaded.dataSO.list<-c(loaded.dataSO.list,loaded.dataSO)
     }else if(length(foundH5) !=0){
       loaded.data <- Read10X_h5(paste(data_dir,"/",filesindir,sep=""), use.names = TRUE, unique.features = TRUE)
@@ -120,23 +118,23 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
       # VlnPlot(loaded.dataSO, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
       # FeatureScatter is typically used to visualize feature-feature relationships, but can be used
       # for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
-
+      
       #plot1 <- FeatureScatter(loaded.dataSO, feature1 = "nCount_RNA", feature2 = "percent.mt")
       plot2 <- FeatureScatter(loaded.dataSO, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-
+      
       plot.list[[index]]<-plot2
       index<-index+1
-
+      
       #Filtering data
       #loaded.dataSO <- subset(loaded.dataSO, subset = nFeature_RNA > 100 & nFeature_RNA < 6500 & percent.mt < 75) #changed this
-
-      #Normalizing the data. Normalized values are stored in UnWounded1[["RNA"]]@data. *
+      
+      #Normalizing the data. 
       loaded.dataSO <- NormalizeData(loaded.dataSO)#, normalization.method = "LogNormalize", scale.factor = 10000)
       #Identification of highly variable features (feature selection) *
       loaded.dataSO <- FindVariableFeatures(loaded.dataSO, selection.method = "vst", nfeatures = 2000)
       loaded.dataSO.list<-c(loaded.dataSO.list,loaded.dataSO)
     }else if(length(foundrds) !=0){
-
+      
       loaded.dataSO<- readRDS(paste(data_dir,"/",filesindir[foundrds],sep=""))
       project_name<-gsub("[\\.\\/]","",data_dir,)
       print(project_name)
@@ -154,18 +152,18 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
       # VlnPlot(loaded.dataSO, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
       # FeatureScatter is typically used to visualize feature-feature relationships, but can be used
       # for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
-
+      
       #plot1 <- FeatureScatter(loaded.dataSO, feature1 = "nCount_RNA", feature2 = "percent.mt")
       plot2 <- FeatureScatter(loaded.dataSO, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-
+      
       plot.list[[index]]<-plot2
       index<-index+1
-
+      
       #Filtering data
       #loaded.dataSO <- subset(loaded.dataSO, subset = nFeature_RNA > 100 & nFeature_RNA < 6500 & percent.mt < 75) #changed this
-
+      
       DefaultAssay(loaded.dataSO)<-"RNA"
-
+      
       #Normalizing the data. *
       #loaded.dataSO <- NormalizeData(loaded.dataSO)#, normalization.method = "LogNormalize", scale.factor = 10000)
       #Identification of highly variable features (feature selection) *
@@ -173,13 +171,13 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
       loaded.dataSO.list<-c(loaded.dataSO.list,loaded.dataSO)
     }
   }
-
+  
   #increase memory size to hold larger objects
   options(future.globals.maxSize = 8000 * 1024^2)
   if(index==2){
     loaded.dataSO.combined <-loaded.dataSO
     jpeg(file=paste(subDir,"/FEATURE_PLOT_",disease,".jpg",sep=""),
-        width=1000, height=800,res=100)
+         width=1000, height=800,res=100)
     plot(plot2)
     dev.off()
   }else{
@@ -223,14 +221,14 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
     plot(p2combined)
     dev.off()
   }
-
+  
   if(annotate==FALSE){
     jpeg(file=paste(subDir,"/UMAP_ANNOT2_",disease,".jpg",sep=""),
          width=1000, height=800,res=100)
     plot(DimPlot(loaded.dataSO.combined, reduction = "umap", split.by = userlabel,group.by = usercelltype,raster=FALSE))
     dev.off()
   }
-
+  
   if(annotate==TRUE){
     # Identify conserved cell type markers
     # For performing differential expression after integration, we switch back to the original
@@ -245,23 +243,23 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
     loaded.dataSO.combined.markerstop1<-loaded.dataSO.combined.markers %>%
       group_by(cluster) %>%
       slice_max(n = 1, order_by = avg_log2FC) #100 gives best results
-
+    
     loaded.dataSO.combined.markerstop2<-loaded.dataSO.combined.markers %>%
       group_by(cluster) %>%
       slice_max(n = 2, order_by = avg_log2FC) #100 gives best results
-
+    
     loaded.dataSO.combined.markerstop100<-loaded.dataSO.combined.markers %>%
       group_by(cluster) %>%
       slice_max(n = 100, order_by = avg_log2FC) #100  gives best results
-
+    
     loaded.dataSO.combined.markerstop200<-loaded.dataSO.combined.markers %>%
       group_by(cluster) %>%
       slice_max(n = 200, order_by = avg_log2FC) #200 gives second best results
-
+    
     loaded.dataSO.combined.markerstop6<-loaded.dataSO.combined.markers %>%
       group_by(cluster) %>%
       slice_max(n = 6, order_by = avg_log2FC) #100 gives best results
-
+    
     genesAll<-as.data.frame(loaded.dataSO.combined.markerstop6$gene)
     unique(loaded.dataSO.combined$seurat_clusters)
     
@@ -277,11 +275,11 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
     plot(VlnPlot(loaded.dataSO.combined, features = loaded.dataSO.combined.markerstop1$gene[1]))
     dev.off()
     #FeaturePlot(loaded.dataSO.combined, features = loaded.dataSO.combined.markerstop1$gene[1],label = T)& theme(legend.position = c(0.1,0.2))
-
+    
     #loaded.dataSO.combined <- ScaleData(loaded.dataSO.combined, verbose = FALSE) #this needs to be removed for JoinLayers
     
     jpeg(file=paste(subDir,"/TOP2-MARKERS_HEAT_",disease,".jpg",sep=""),
-        width=1000, height=800,res=100)
+         width=1000, height=800,res=100)
     plot(DoHeatmap(loaded.dataSO.combined, features = loaded.dataSO.combined.markerstop2$gene) + NoLegend())
     dev.off()
     #Run Enrich R on top 100 Markers
@@ -310,7 +308,7 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
       if (websiteLive) plot(plotEnrich(enriched[[paste("PanglaoDB_Augmented_2021_",m,sep="")]],  numChar = 40, y = "Count", orderBy = "P.value",title = paste("PanglaoDB",m))+plotEnrich(enriched[[paste("CellMarker_Augmented_2021_",m,sep="")]], numChar = 40, y = "Count", orderBy = "P.value",title = paste("CellMarker",m))+plotEnrich(enriched[[paste("Tabula_Sapiens_",m,sep="")]], numChar = 40, y = "Count", orderBy = "P.value",title = paste("Tabula_Sapiens",m)))
       dev.off()
     }
-
+    
     #Rename indents
     new.cluster.ids <- make.names(annotatedclusters,unique = T)
     names(new.cluster.ids) <- levels(loaded.dataSO.combined)
@@ -323,7 +321,7 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
     loaded.dataSO.combined$celltype.label <- paste(Idents(loaded.dataSO.combined), gsub("[0-9]+","",loaded.dataSO.combined$orig.ident), sep = "_")
     loaded.dataSO.combined$label <- gsub("[0-9]+","",loaded.dataSO.combined$orig.ident)
     loaded.dataSO.combined$celltype <- Idents(loaded.dataSO.combined)
-
+    
     jpeg(file=paste(subDir,"/UMAP_ANNOT3_SPLIT_",disease,".jpg",sep=""),
          width=1000, height=800,res=100)  
     plot(DimPlot(loaded.dataSO.combined, reduction = "umap", split.by = "label",label = TRUE))
@@ -350,24 +348,24 @@ runBasicAnalysis<-function(disease,path,annotate=TRUE,userlabel,usercelltype){#s
     plot(VlnPlot(loaded.dataSO.combined, features = c(loaded.dataSO.combined.markerstop1$gene[1]),split.by = "label",split.plot = TRUE))
     dev.off()
   }
-
-
+  
+  
   # What proportion of cells are in each cluster?
   jpeg(file=paste(subDir,"/ALL_CELLS_PROP_",disease,".jpg",sep=""),
        width=1000, height=800,res=100)
   pie(prop.table(table(loaded.dataSO.combined[[usercelltype]][,1])),main = "Proportion of cells per cell-type")
   dev.off()
   #prop.table(table(Idents(loaded.dataSO.combined))) #same result
-
+  
   # How does cluster membership vary by condition?
   tableofcellconts<-table(loaded.dataSO.combined[[usercelltype]][,1], loaded.dataSO.combined[[userlabel]][,1])
-
+  
   # What proportion of cells are in each cluster by condition
   proportions<-as.data.frame(prop.table(table(loaded.dataSO.combined[[usercelltype]][,1], loaded.dataSO.combined[[userlabel]][,1]), margin = 2))
   #proportions1<-as.data.frame(prop.table(table(loaded.dataSO.combined$celltype, loaded.dataSO.combined$label), margin = 2))
-
+  
   write.table(proportions,"proportionsFinalPac.txt",quote = F,row.names = T,sep = "\t")
-
+  
   #listofoutput<-list(loaded.dataSO.combined,termsKEGG,termsGO,termsMSIG,termsWiki,termsReact,termsMOA)
   return(loaded.dataSO.combined)
 }
